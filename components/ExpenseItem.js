c6fda5f4a1f8c6a42a82665b526091ea7acd4ed6
John Doe <sailalith1993@gmail.com>
2022-06-30 15:54:09 +0530

    first commit

---
+++ components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense items!</h2>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Food Rs 10</p>
+    </div>
+  );
+}
+export default ExpenseItem;
