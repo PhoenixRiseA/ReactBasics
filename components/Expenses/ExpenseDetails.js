6b67ec597ef3f76a17c090bb6fc752b8eb34eb69
John Doe <sailalith1993@gmail.com>
2022-07-01 22:55:19 +0530

    Transforming to fat arrow functions

--- components/Expenses/ExpenseDetails.js
+++ components/Expenses/ExpenseDetails.js
@@ -1,4 +1,4 @@
-function ExpenseDetails(props) {
+const ExpenseDetails = (props) => {
   return (
     <div className="expense-item__description">
       <h2>{props.title}</h2>
@@ -6,6 +6,6 @@ function ExpenseDetails(props) {
       <h2>{props.location}</h2>
     </div>
   );
-}
+};
 
 export default ExpenseDetails;
--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,13 +1,19 @@
+import React from "react";
 import "./ExpenseItem.css";
+// import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails amount={props.amount}></ExpenseDetails>
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        location={props.location}
+      ></ExpenseDetails>
     </div>
   );
-}
+};
 export default ExpenseItem;
